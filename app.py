3a5710e1feaaf721d2ee13207e07397c07e36b37
Lucas-25200 <115980120+Lucas-25200@users.noreply.github.com>
2023-09-21 22:29:17 +0200

    push

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from PIL import Image
 import pytesseract
+import os
 
 app = Flask(__name__)
+
+# Emplacement du répertoire contenant les images/documents
+dossier_images = '/Image RMT'
 
 @app.route('/')
 def index():
@@ -12,13 +16,23 @@ def index():
 def recherche():
     recherche_texte = request.form['recherche']
 
-    # Utilisez ici votre propre logique pour rechercher le texte dans les images.
-    # Par exemple, vous pouvez parcourir un répertoire d'images et extraire le texte de chacune.
+    # Parcourez le répertoire des images/documents et recherchez le texte correspondant
+    for filename in os.listdir(dossier_images):
+        if filename.endswith('.png') or filename.endswith('.jpg'):
+            image_path = os.path.join(dossier_images, filename)
+            texte_extrait = pytesseract.image_to_string(Image.open(image_path))
+            if recherche_texte in texte_extrait:
+                # Redirigez vers l'image correspondante
+                return redirect(url_for('image', nom_image=filename))
 
-    # Placeholder : Remplacez ce code par votre propre logique d'OCR.
-    texte_extrait = "Texte extrait de l'image."
+    # Si aucune correspondance n'est trouvée, affichez un message d'erreur
+    return render_template('resultats.html', erreur="Aucune correspondance trouvée.")
 
-    return render_template('resultats.html', texte_extrait=texte_extrait)
+@app.route('/image/<nom_image>')
+def image(nom_image):
+    # Récupérez l'image correspondante et affichez-la
+    image_path = os.path.join(dossier_images, nom_image)
+    return render_template('image.html', image_path=image_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
